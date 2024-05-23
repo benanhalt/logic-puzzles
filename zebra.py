642c8f3faff61715be3cdbdd1e9ea98c4bf21dba
Ben Anhalt <ben.anhalt@gmail.com>
2024-05-23 18:29:44 -0500

    Using quantifiers.

--- zebra.py
+++ zebra.py
@@ -9,10 +9,8 @@ Nation, (england, spain, ukraine, norway, japan) = \
 Beverage, (coffee, tea, milk, juice, water) = \
     EnumSort("Drink", "coffee tea milk juice water".split())
 
-
 Pet, (dog, snails, fox, horse, zebra) = \
     EnumSort("Pet", "dog snails fox horse zebra".split())
-
 
 Smoke, (oldgold, kools, chesterfields, luckystrikes, parliaments) = \
     EnumSort("Smoke", "oldgold kools chesterfields luckystrikes parliaments".split())
@@ -27,104 +25,78 @@ smokes = Function("smokes", House, Smoke)
 
 house_number = Function("house_number", House, IntSort())
 
+h, g = Consts('h g', House)
+
 solver = Solver()
-
-# In the interest of clarity, it must be added that each of the five
-# houses is painted a different color, and their inhabitants are of
-# different national extractions, own different pets, drink different
-# beverages and smoke different brands of American cigarets [sic].
-solver.add(Distinct([house_number(h) for h in houses]))
-solver.add(Distinct([nationality(h) for h in houses]))
-solver.add(Distinct([pet(h) for h in houses]))
-solver.add(Distinct([drinks(h) for h in houses]))
-solver.add(Distinct([smokes(h) for h in houses]))
-
-
-# 1. There are five houses.
-solver.add([
-    And(house_number(h) >= 1, house_number(h) <=5)
-    for h in houses
-])
-
-# 2. The Englishman lives in the red house.
-solver.add(nationality(red_house) == england)
-
-# 3. The Spaniard owns the dog.
-the_spaniards_house = Const("the_spaniards_house", House)
-solver.add(nationality(the_spaniards_house) == spain)
-solver.add(pet(the_spaniards_house) == dog)
-
-# 4. Coffee is drunk in the green house.
-solver.add(drinks(green_house) == coffee)
-
-# 5. The Ukrainian drinks tea.
-the_ukrainians_house = Const("the_ukrainians_house", House)
-solver.add(nationality(the_ukrainians_house) == ukraine)
-solver.add(drinks(the_ukrainians_house) == tea)
-
-# 6. The green house is immediately to the right of the ivory house.
-solver.add(house_number(green_house) == house_number(ivory_house) + 1)
-
-# 7. The Old Gold smoker owns snails.
-the_oldgold_house = Const("the_oldgold_house", House)
-solver.add(smokes(the_oldgold_house) == oldgold)
-solver.add(pet(the_oldgold_house) == snails)
-
-# 8. Kools are smoked in the yellow house.
-solver.add(smokes(yellow_house) == kools)
-
-# 9. Milk is drunk in the middle house.
-the_middle_house = Const("the_middle_house", House)
-solver.add(house_number(the_middle_house) == 3)
-solver.add(drinks(the_middle_house) == milk)
-
-# 10. The Norwegian lives in the first house.
-the_first_house = Const("the_first_house", House)
-solver.add(house_number(the_first_house) == 1)
-solver.add(nationality(the_first_house) == norway)
-
-# 11. The man who smokes Chesterfields lives in the house next to the man with the fox.
-the_chesterfield_house = Const("the_chesterfield_house", House)
-the_fox_house = Const("the_fox_house", House)
-solver.add(smokes(the_chesterfield_house) == chesterfields)
-solver.add(pet(the_fox_house) == fox)
 solver.add(
-    Or(
-        house_number(the_fox_house) == house_number(the_chesterfield_house) + 1,
-        house_number(the_fox_house) == house_number(the_chesterfield_house) - 1,
-    )
-)
+    # 1. There are five houses.
+    ForAll(h, And(house_number(h) >= 1, house_number(h) <=5)),
 
-# 12. Kools are smoked in the house next to the house where the horse is kept.
-the_kools_house = Const("the_kools_house", House)
-the_horse_house = Const("the_horse_house", House)
-solver.add(smokes(the_kools_house) == kools)
-solver.add(pet(the_horse_house) == horse)
-solver.add(
-    Or(
-        house_number(the_horse_house) == house_number(the_kools_house) + 1,
-        house_number(the_horse_house) == house_number(the_kools_house) - 1,
-    )
-)
+    # 2. The Englishman lives in the red house.
+    nationality(red_house) == england,
 
-# 13. The Lucky Strike smoker drinks orange juice.
-the_luckystrikes_house = Const("the_luckystrikes_house", House)
-solver.add(smokes(the_luckystrikes_house) == luckystrikes)
-solver.add(drinks(the_luckystrikes_house) == juice)
+    # 3. The Spaniard owns the dog.
+    ForAll(h, Implies(nationality(h) == spain, pet(h) == dog)),
 
-# 14. The Japanese smokes Parliaments.
-the_japanese_house = Const("the_japanese_house", House)
-solver.add(nationality(the_japanese_house) == japan)
-solver.add(smokes(the_japanese_house) == parliaments)
+    # 4. Coffee is drunk in the green house.
+    drinks(green_house) == coffee,
 
-# 15. The Norwegian lives next to the blue house.
-the_norwegian_house = Const("the_norwegian_house", House)
-solver.add(nationality(the_norwegian_house) == norway)
-solver.add(
-    Or(
-        house_number(the_norwegian_house) == house_number(blue_house) + 1,
-        house_number(the_norwegian_house) == house_number(blue_house) - 1,
-    )
+    # 5. The Ukrainian drinks tea.
+    ForAll(h, Implies(nationality(h) == ukraine, drinks(h) == tea)),
+
+    # 6. The green house is immediately to the right of the ivory house.
+    house_number(green_house) == house_number(ivory_house) + 1,
+
+    # 7. The Old Gold smoker owns snails.
+    ForAll(h, Implies(smokes(h) == oldgold, pet(h) == snails)),
+
+    # 8. Kools are smoked in the yellow house.
+    smokes(yellow_house) == kools,
+
+    # 9. Milk is drunk in the middle house.
+    ForAll(h, Implies(house_number(h) == 3, drinks(h) == milk)),
+
+    # 10. The Norwegian lives in the first house.
+    ForAll(h, Implies(nationality(h) == norway, house_number(h) == 1)),
+
+    # 11. The man who smokes Chesterfields lives in the house next to the man with the fox.
+    ForAll([h, g], Implies(
+        And(smokes(h) == chesterfields, pet(g) == fox),
+        Or(house_number(h) == house_number(g) + 1, house_number(h) == house_number(g) - 1)
+    )),
+
+    # 12. Kools are smoked in the house next to the house where the horse is kept.
+    ForAll([h, g], Implies(
+        And(smokes(h) == kools, pet(g) == horse),
+        Or(house_number(h) == house_number(g) + 1, house_number(h) == house_number(g) - 1)
+    )),
+
+    # 13. The Lucky Strike smoker drinks orange juice.
+    ForAll(h, Implies(smokes(h) == luckystrikes, drinks(h) == juice)),
+
+    # 14. The Japanese smokes Parliaments.
+    ForAll(h, Implies(nationality(h) == japan, smokes(h) == parliaments)),
+
+    # 15. The Norwegian lives next to the blue house.
+    ForAll(h, Implies(
+        nationality(h) == norway,
+        Or(house_number(h) == house_number(blue_house) + 1, house_number(h) == house_number(blue_house) - 1)
+    )),
+
+    # In the interest of clarity, it must be added that each of the five
+    # houses is painted a different color, and their inhabitants are of
+    # different national extractions, own different pets, drink different
+    # beverages and smoke different brands of American cigarets [sic].
+    ForAll([h, g], Implies(
+        h != g,
+        And(
+            house_number(h) != house_number(g),
+            nationality(h) != nationality(g),
+            pet(h) != pet(g),
+            drinks(h) != drinks(g),
+            smokes(h) != smokes(g),
+        )
+    ))
 )
 
 # Now, who drinks water? Who owns the zebra?
@@ -133,16 +105,6 @@ the_water_house = Const("the_water_house", House)
 the_zebra_house = Const("the_zebra_house", House)
 solver.add(drinks(the_water_house) == water)
 solver.add(pet(the_zebra_house) == zebra)
-
-# In the interest of clarity, it must be added that each of the five
-# houses is painted a different color, and their inhabitants are of
-# different national extractions, own different pets, drink different
-# beverages and smoke different brands of American cigarets [sic].
-solver.add(Distinct([nationality(h) for h in houses]))
-solver.add(Distinct([drinks(h) for h in houses]))
-solver.add(Distinct([pet(h) for h in houses]))
-solver.add(Distinct([smokes(h) for h in houses]))
-solver.add(Distinct([house_number(h) for h in houses]))
 
 assert solver.check() == sat
 
